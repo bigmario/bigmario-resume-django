eb36df03c7e7235638af853c3411520b6aa929da
Mario Castro <mariocastro.pva@gmail.com>
2022-06-01 09:40:55 -0400

    fix CSRF on settings

--- bigmario_resume/settings.py
+++ bigmario_resume/settings.py
@@ -27,6 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
+CSRF_TRUSTED_ORIGINS = ["https://bigmario-resume.herokuapp.com"]
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

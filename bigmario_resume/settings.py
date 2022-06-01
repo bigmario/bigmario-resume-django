20959f7b4a543e13998bf2062fbab18f5f8a80d0
Mario Castro <mariocastro.pva@gmail.com>
2022-06-01 09:18:49 -0400

    Automatic configuration with django-heroku

--- bigmario_resume/settings.py
+++ bigmario_resume/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import django_on_heroku
+
+django_on_heroku.settings(locals())
 
 load_dotenv()
 

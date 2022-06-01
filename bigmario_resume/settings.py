ee8bcc46371640ce701e6ee5b79642ce7798c9ef
Mario Castro <mariocastro.pva@gmail.com>
2022-06-01 09:32:01 -0400

    fix

--- bigmario_resume/settings.py
+++ bigmario_resume/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 from dotenv import load_dotenv
 import django_on_heroku
 
-django_on_heroku.settings(locals())
-
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -137,3 +135,5 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+django_on_heroku.settings(locals())

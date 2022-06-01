73535117aa420cbf6183906f81264c6743a553c8
Mario Castro <mariocastro.pva@gmail.com>
2022-06-01 10:33:15 -0400

    modifications in settings

--- bigmario_resume/settings.py
+++ bigmario_resume/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "media"]
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "mediafiles"]
 
-STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"

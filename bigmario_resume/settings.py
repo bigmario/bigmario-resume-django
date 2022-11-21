5c631cde3a4362d750e5248ec16f99ce6d83138a
Mario Castro <mariocastro.pva@gmail.com>
2022-11-21 15:00:41 -0400

    static

--- bigmario_resume/settings.py
+++ bigmario_resume/settings.py
@@ -145,13 +145,13 @@ if not DEBUG:
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-# MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
+    # MEDIA_URL = "/media/"
+    MEDIA_ROOT = BASE_DIR / "mediafiles"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "mediafiles"),
-]
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+        os.path.join(BASE_DIR, "mediafiles"),
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

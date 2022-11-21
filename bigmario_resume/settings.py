075f196384d43b3b6c44e21348755709b30906d6
Mario Castro <mariocastro.pva@gmail.com>
2022-11-21 15:14:34 -0400

    static files

--- bigmario_resume/settings.py
+++ bigmario_resume/settings.py
@@ -135,23 +135,26 @@ USE_TZ = True
 STATIC_URL = "/static/"
 # STATIC_ROOT = BASE_DIR / "staticfiles"
 
+MEDIA_URL = "/media/"
+
 # Following settings only make sense on production and may break development environments.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "media"),
+    os.path.join(BASE_DIR, "mediafiles"),
+]
+
 if not DEBUG:
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+    MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-    # MEDIA_URL = "/media/"
-    MEDIA_ROOT = BASE_DIR / "mediafiles"
-
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-        os.path.join(BASE_DIR, "mediafiles"),
-    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
